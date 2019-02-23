432bb96a319385c07d3a5f6f62fa1cfdffc5c056
Nick Ruof <nickruof@Nicks-MacBook-Pro.local>
2019-02-23 14:44:15 -0800

    Additions to data_loaders and digitizers

--- SiPMStudio/core/data_loading.py
+++ SiPMStudio/core/data_loading.py
@@ -5,24 +5,25 @@ from abc import ABC
 
 class DataLoader(ABC):
 
-	def __init__(self, df_data=None):
+    def __init__(self, df_data=None):
 
-		if df_data is not None:
-			self.load_data(df_data)
-		else:
-			self.df_data = None
+        if df_data is not None:
+            self.load_data(df_data)
+        else:
+            self.df_data = None
 
-	def load_data(self, df_data):
+    def load_data(self, df_data):
 
-		if isinstance(df_data, pd.core.frame.DataFrame):
-			self.df_data = df_data
-		elif isinstance(df_data, str):
-			self.df_data = pd.read_csv(df_data, delimiter=";", header=None, skiprows=1)
-		else:
-			raise TypeError("DataType not recognized!")
+        if isinstance(df_data, pd.core.frame.DataFrame):
+            self.df_data = df_data
+        elif isinstance(df_data, str):
+            self.df_data = pd.read_csv(df_data, delimiter=";", header=None, skiprows=1)
+        else:
+            raise TypeError("DataType not recognized!")
 
-	def format_data(self, columns):
-		self.df_data.columns = columns
+    @classMethod
+    def format_data(self, columns):
+        self.df_data.columns = columns
 
-	def clear_data(self):
-		self.df_data = None
+    def clear_data(self):
+        self.df_data = None
--- SiPMStudio/core/digitizers.py
+++ SiPMStudio/core/digitizers.py
@@ -6,8 +6,46 @@ from .data_loading import DataLoader
 
 class Digitizer(DataLoader):
 
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs):
 
-	def apply_settings(self, settings):
-		
+    def apply_settings(self, settings):
+
+        if settings["digitizer"] == self.decoder_name:
+            sk = settings.keys()
+            if "window" in sk:
+                self.window = True
+                self.win_type = settings["window"]
+            if "n_samp" in sk:
+                self.n_samp = settings["n_samp"]
+            if "n_blsamp" in sk:
+                self.n_blsamp = settings["n_blsamp"]
+
+class CAENDT5730(Digitizer):
+
+    def __init__(self, *args, **kwargs):
+
+        self.id = None
+        self.model_name = "CAENDT5730"
+        self.adc_bitcount = 14
+        self.sample_time = None
+        self.waves = False
+
+        self.parameters = ["Timestamp", "E_short", "E_long"]
+        super().__init__(*args, **kwargs):
+
+
+    def format_data(self):
+        if self.waves:
+            params_frame = self.df_data.iloc[:, :3]
+            params_frame.columns = self.parameters
+            waveforms = csv.iloc[:, 4:].copy()
+            waveforms = self.waveforms.transpose().set_index(keys=np.array(range(0, 2*self.waveforms.shape[1], 2)))
+            return params_frame, waveforms
+        else:
+            params = self.df_data.iloc[:, :3]
+            self.params_frame = pd.DataFrame(params, columns=["timetag", "E_short", "E_long"])
+            return params
+
+    def parse_xml(self, xmlfile):
+        
