66d271408cec03648db4d118d7f996bae56301df
Nick Ruof <nickruof@D-10-19-20-85.dhcp4.washington.edu>
2019-03-06 12:49:17 -0800

    Syntax fixes

--- SiPMStudio/core/digitizers.py
+++ SiPMStudio/core/digitizers.py
@@ -45,9 +45,9 @@ class CAENDT5730(Digitizer):
         if waves:
             params_frame = self.df_data.iloc[:, :3]
             params_frame.columns = self.parameters
-            waveforms = self.df_data.iloc[:, 4:].copy()
-            waveforms = waveforms.transpose().set_index(keys=np.array(range(0, 2*self.waveforms.shape[1], 2)))
-            return waveforms
+            waves_frame = self.df_data.iloc[:, 4:].copy()
+            waves_frame = waves_frame.transpose().set_index(keys=np.array(range(0, 2*waves_frame.shape[1], 2)))
+            return waves_frame
         else:
             params = self.df_data.iloc[:, :3]
             params_frame = pd.DataFrame(params, columns=self.parameters)
