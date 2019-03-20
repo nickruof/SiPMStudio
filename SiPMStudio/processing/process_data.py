077175ed2388d4f931a58fafe17c0d87391c288f
Nick Ruof <nickruof@Nicks-MacBook-Pro.local>
2019-03-19 21:34:17 -0700

    Multiprocessing and Progress Bar functionality in process_data

--- SiPMStudio/processing/process_data.py
+++ SiPMStudio/processing/process_data.py
@@ -1,4 +1,5 @@
 import os, re, sys, time
+import tqdm
 import numpy as np
 import pandas as pd
 import multiprocessing as mp
@@ -20,20 +21,31 @@ def ProcessData(data_file,
     in_dir = os.path.dirname(data_file)
     output_dir = os.getcwd() if output_dir is None else output_dir
 
+    CHUNKSIZE = chunk
+    NCPU = mp.cpu_count()
+
     #Declare an output file and overwriting options here
 
-    for d in digitizers:
+    for d in tqdm(digitizers):
         processor.digitizer = d
         d.load_data(df_data=data_file, chuncksize=chunk)
-        for block in d.df_data:
-            block.drop(["FLAGS"], axis=1)
-            [processor.calcs, processor.waves] = np.split(block, [3])
-            processor.process()
-            new_chunk = pd.concat([processor.calcs, processor.waves], axis=1)
-            df_result = pd.concat([df_result, new_chunk])
+        result_list = pd.DataFrame()
+        for block in tqdm(d.df_data):
+            if multiprocess:
+                with mp.Pool(NCPU) as p:
+                    new_chunk = p.map(partial(process_chunk, block, processor))
+                result_list = pd.concat([result_list, new_chunk])
+            else:
+                new_chunk = process_chunk(block, processor)
+                result_list = pd.concat([result_list, new_chunk])
 
 
 
+def process_chunk(df_data, processor):
+    df_data = df_data.drop(["FLAGS"], axis=1)
+    [processor.calcs, processor.waves] = np.split(blocks, [3])
+    processor.process()
+    return pd.concat([processor.calcs, processor.waves], axis=1)
 
     
 
