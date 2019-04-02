bd0f7bf6b12b460e446e0da32931c3077fdf0665
Nick Ruof <nickruof@D-10-19-15-255.dhcp4.washington.edu>
2019-04-02 16:59:07 -0700

    Name changes and experiment processor

--- SiPMStudio/processing/experiment.py
+++ SiPMStudio/processing/experiment.py
@@ -1,63 +1,23 @@
+import os, re, sys, time
+import math
+import tqdm
+import animation
 import numpy as np
 import pandas as pd
-from abc import ABC
-import sys
+import multiprocessing as mp
 
-import SiPMStudio.core.devices as gadget
-import SiPMStudio.analysis.dark as sith
-import SiPMStudio.analysis.light as jedi
+from SiPMStudio.core import data_loading
+from SiPMStudio.core import digitizers
+from SiPMStudio.core import devices
 
-class Experiment(ABC):
+from functools import partial
+from pathos.threading import ThreadPool
 
-    def __init__(self, settings=None):
-        self.measurement_list = []
-        self.settings = {}
-
-        if settings is not None:
-            self.settings = settings
-        for key in settings:
-            self.add(key, settings[key])
-
-
-    def process(self):
-        for measurment in self.measurement_list:
-            if isinstance(measurement, Measurement):
-                p_result = measurement.process_block()
-            else:
-                print("Unknown Measurment type!")
-
-    def add(self, fun_name, settings={}):
-        if fun_name in self.settings:
-            self.settings[fun_name] = {**self.settings[fun_name], **settings}
-        else:
-            self.settings[fun_name] = settings
-
-        if fun_name in dir(sith):
-            self.measurement_list.append(
-                Measurement(getattr(sith, fun_name), self.settings[fun_name]))
-        elif fun_name in dir(jedi)
-            self.measurement_list.append(
-                Measurement(getattr(jedi, fun_name), self.settings[fun_name]))
-        else:
-            print("ERROR! unknown function: ", fun_name)
-            sys.exit()
-
-class Measurment:
-
-    def __init__(self, function, fun_args={}):
-        self.function = function
-        self.fun_args = fun_args
-
-    def process_block(self):
-        return self.function(**self.fun_args)
-
-class UtilityBelt:
-
-    def __init__(self, gadgets={}):
-        self.gadgets = gadgets
-
-    def add(self, gadget_name, gadget_object):
-        self.gadgets[gadget_name] = gadget_object
-        
-
-
+def Experiment(data_files, 
+                measurement_array, 
+                digitizer, 
+                output_dir=None, 
+                overwrite=True,
+                multiprocess=True, 
+                verbose=False,
+                chunk=2000):
