b3611996c8fd0200316a9ff0716a65cf9b742fdb
nickruof <nickruof@uw.edu>
2019-08-16 17:17:19 -0700

    Added to noise analysis

--- SiPMStudio/analysis/noise.py
+++ SiPMStudio/analysis/noise.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from scipy import fftpack
+from scipy.integrate import trapz
 
 
 def noise_spectrum(waves_data, digitizer):
@@ -11,4 +12,17 @@ def noise_spectrum(waves_data, digitizer):
     power_spec = 2.0/num_samples * np.abs(waves_data_fft[:num_samples//2])
     return frequencies, power_spec
 
+
+def average_power(waveform):
+    time = np.linspace(0, 2*len(waveform), len(waveform))
+    waveform_squared = np.multiply(waveform, waveform)
+    average = (1/len(time)) * trapz(waveform_squared, time)
+    return average
+
+
+def snr(waveform, noise):
+    noise_average = average_power(noise)
+    signal_average = average_power(waveform)
+    signal_to_noise = 10 * np.log10((signal_average-noise_average) / noise_average)
+    return signal_to_noise
 
