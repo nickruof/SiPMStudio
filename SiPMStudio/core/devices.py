bad3d345f81f5c39029ac48d2628d3bd32db1c90
nickruof <nickruof@uw.edu>
2021-03-12 12:04:14 -0800

    Refactor core Devices for light measurements and analysis updates

--- SiPMStudio/core/devices.py
+++ SiPMStudio/core/devices.py
@@ -7,6 +7,8 @@ import pandas as pd
 import uncertainties
 
 from scipy.stats import linregress
+from scipy.interpolate import interp1d
+from scipy.integrate import quad
 import scipy.constants as const
 import SiPMStudio.plots.plots_base as plt_base
 
@@ -14,56 +16,79 @@ from SiPMStudio.analysis.dark import current_waveforms, integrate_current
 
 
 class Sipm:
-    def __init__(self, settings, data_names=None):
+    def __init__(self, model, settings, dark_trig_settings=None, light_trig_settings=None, data_names=None):
         self.settings = None
+        self.settings["dark_trig"] = dark_trig_settings
+        self.settings["light_trig"] = light_trig_settings
         with open(settings, "r") as json_file:
-            self.settings = json.load(json_file)
+            self.settings["dark"] = json.load(json_file)
 
-        if data_names:
-            self.data_names = data_names
-        else:
-            data_names = ["gain", "dark_rate", "cross_talk", "afterpulse"]
+        self.dark_names = ["gain", "dark_rate", "cross_talk", "afterpulse"]
+        self.trig_names = ["n_ped", "n_total"]
 
-        self.t1_list = glob.glob(self.settings["output_path_t1"] + "/*.h5").sort()
-        self.file_list = glob.glob(self.settings["output_path_t2"] + "/*.h5").sort()
-        self.bias_voltages = []
-        self.data_names = data_names
-        self.data_dict = {}
+        self.files_dict = {"dark": {"t1": None, "t2": None}, "dark_trig": {"t1": None, "t2": None},
+                           "light_trig": {"t1": None, "t2": None}}
+        self.bias_voltages = {"dark": [], "dark_trig": [], "light_trig": []}
+        self.data_dict = {"dark": {}, "dark_trig": {}, "light_trig": {}}
+
+        self.model_name = ""
         self.breakdown = 0
         self.micro_cap = 0
+        self.area = 0
 
+        self.initialize_files()
         self.initialize_data()
 
+    def initialize_files(self):
+        file_types = ["dark", "dark_trig", "light_trig"]
+        for file in file_types:
+            self.settings[file]["t1"] = glob.glob(self.settings[file]["output_path_t1"] + "/*.h5").sort()
+            self.settings[file]["t2"] = glob.glob(self.settings[file]["output_path_t2"] + "/*.h5").sort()
+
     def initialize_data(self):
-        for file_name in self.file_list:
-            h5_file = h5py.File(file_name, "r")
+        file_types = ["dark", "dark_trig", "light_trig"]
+        for file in file_types:
+            param_names = None
+            if file == "dark":
+                param_names = self.dark_names
+            elif file.endswith("trig"):
+                param_names = self.trig_names
+            else:
+                print("Found unknown file type name in initialize data!")
+                break
+            self._load_data(file, "t1", param_names)
+            self._load_data(file, "t2", param_names)
+
+    @staticmethod
+    def _load_data(self, file_type, tier, param_names):
+        for file in self.settings[file_type][tier]:
+            h5_file = h5py.File(file, "r")
             bias = h5_file["bias"][()]
-            self.bias_voltages.append(bias)
             data_element = {}
-            for data in self.data_names:
+            for data in param_names:
                 dataset = h5_file[data]
                 data_element[data] = dataset[()]
-            self.data_dict[str(bias)] = data_element
-
-    def __getitem__(self, name):
-        bias_voltages = list(self.data_dict.keys())
-        return_data = []
-        for bias in bias_voltages:
-            return_data.append(self.data_dict[bias][name])
-        over_volts = np.array(list(map(float, bias_voltages))) - self.breakdown
-        return over_volts, np.array(return_data)
+            self.data[file_type][bias] = data_element
+            self.bias_voltages[file_type].append(bias)
 
     def __str__(self):
         return str(self.data_dict)
 
-    def ph_histogram(self, ax, bias_voltage, file_type="t1", bins=1000, density=False, log=True):
+    def __getitem__(self, item):
+        data_type, param = item
+        voltages = list(self.data_dict[data_type].keys())
+        param_data = []
+        for voltage in voltages:
+            param_data.append(self.data_dict[data_type][voltage][param])
+        return np.array(voltages), np.array(param_data)
+
+    def ph_histogram(self, ax, bias_voltage, tier="t1", bins=1000, density=False, log=True):
         waveforms = None
-        index = self.bias_voltages.index(bias_voltage)
-        if file_type == "t1":
-            h5_file = h5py.File(self.t1_list[index], "r")
+        index = self.bias_voltages["dark"].index(bias_voltage)
+        h5_file = h5py.File(self.files_dict["dark"][tier][index], "r")
+        if tier == "t1":
             waveforms = h5_file["/raw/waveforms"][:] - h5_file["/raw/baselines"][:]
-        elif file_type == "t2":
-            h5_file = h5py.File(self.file_list[index], "r")
+        elif tier == "t2":
             waveforms = h5_file["/processed/waveforms"][:]
         else:
             raise AttributeError("Unrecognized file type: " + str(file_type))
@@ -76,8 +101,8 @@ class Sipm:
             ax.set_yscale("log")
 
 
-def extrapolate_breakdown(sipm):
-    bias, gains = sipm["gain"]
+def extrapolate_breakdown(sipm, data="dark"):
+    bias, gains = sipm[[data, "gain"]]
     fit_data = linregress(bias, gains)
     slope = fit_data[0]
     intercept = fit_data[1]
@@ -88,17 +113,22 @@ def extrapolate_breakdown(sipm):
 class Photodiode:
 
     def __init__(self, name, area):
-        self.brand = name
+        self.model_name = name
         self.area = area
         self.bias = None
         self.current = []
         self.responsivity = pd.DataFrame()
-        self.cal_slope = 1
+        self.data = None
+        self.cal_func = None
 
     def load_response(self, file_path):
         response_data = pd.read_csv(file_path, delimiter=", ", header=None, engine="python")
         self.responsivity["wavelength"] = np.multiply(response_data[0], 1e-9)
         self.responsivity["responsivity"] = response_data[1]
+
+    def load_data(self, file_path):
+        columns = ["bias", "dark", "dark_err", "light", "light_err"]
+        self.data = pd.read_csv(file_path, delimiter=" ", header=None, columns=columns)
 
     def get_response(self, wavelength):
         if self.responsivity.empty:
@@ -109,15 +139,31 @@ class Photodiode:
             return np.interp(x=wavelength, xp=self.responsivity["wavelength"], fp=self.responsivity["responsivity"])
 
     def calibrate(self, light_source):
-        response = self.get_response(light_source.wavelength)
-        energy_per_photon = const.h*const.c / light_source.wavelength
-        self.cal_slope = 1 / (energy_per_photon * response * self.area)
+        bias = self.data["bias"].to_numpy()
+        photo_current = (self.data["light"] - self.data["dark"]).to_numpy()
+        fit_data = linregress(bias, photo_current)
+        slope, intercept = fit_data[0], fit_data[1]
 
-    def photon_rate(self, current, active_area):
-        return active_area * self.cal_slope*current*1.0e-9
+        def func(voltage):
+            return slope*voltage + intercept
+
+        self.cal_func = func
 
 
-class Led:
-    def __init__(self, name, wavelength):
-        self.name = name
-        self.wavelength = wavelength
+class LightSource:
+
+    def __init__(self, name, wavelengths, intensity_profile):
+        self.model_name = name
+        self.wavelengths = wavelengths
+        self.intensity_profile = intensity_profile
+        self.profile = interp1d(wavelengths, intensity_profile, kind="linear")
+        self.profile_norm = 1
+
+    def calculate_norm(self, spacing=0.1):
+        num_points = (max(self.wavelengths) - min(self.wavelengths) ) / spacing
+        x = np.linspace(min(self.wavelengths), max(self.wavelengths), round(num_points))
+        y = self.profile(x)
+        self.profile_norm = np.cumsum(y) * spacing
+
+    def norm_profile(self, wavelength):
+        return self.profile(wavelength) / self.profile_norm
