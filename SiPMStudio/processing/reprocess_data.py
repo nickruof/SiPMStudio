3221b050d8424d5d85d98f042ad53bdf20f12801
nickruof <nickruof@uw.edu>
2021-09-29 16:33:32 -0700

    Principle outline of reprocess_data

--- SiPMStudio/processing/reprocess_data.py
+++ SiPMStudio/processing/reprocess_data.py
@@ -3,6 +3,21 @@ import tqdm
 import h5py
 import numpy as np
 
+from SiPMStudio.processing.process_data import _chunk_range
+
+def data_chunk(h5_file, begin, end):
+    storage = {}
+    for name in h5_file["/raw"].keys():
+        storage[f"/raw/{name}"] = h5_file[f"/raw/{name}"][begin:end]
+
+    for name in h5_file["/processed"].keys():
+        storage[f"/processed/{name}"] = h5_file[f"/processed/{name}"][begin:end]
+    return storage
+
+def output_chunk(output, h5_file, begin, end):
+    for key, value in output.items():
+        h5_file[key][begin:end] = value
+
 
 def reprocess_data(settings, processor, verbose=False, chunk=2000, write_size=1):
     path_t2 = settings["output_path_t2"]
@@ -15,6 +30,15 @@ def reprocess_data(settings, processor, verbose=False, chunk=2000, write_size=1)
 
     for idx, file in enumerate(output_files):
         destination = os.path.join(path_t2, file)
-        h5_file = h5py.File(destination, "r")
+        h5_file = h5py.File(destination, "r+")
         num_rows = h5_file["/raw/waveforms"][:].shape[0]
-        df_storage = {}
+        for i in tqdm.tqdm(range(num_rows//chunk + 1)):
+            begin, end = _chunk_range(i, chunk, num_rows)
+            storage = data_chunk(h5_file, begin, end)
+            output_storage = _process_chunk(storage, processor)
+        h5_file.close()
+
+
+def _process_chunk(storage, processor):
+    processor.init_outputs(storage)
+    return processor.process()
