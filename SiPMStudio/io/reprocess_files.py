656282bd4fa8e32580e9cdf1062bab23425632e0
nickruof <nickruof@uw.edu>
2021-09-29 17:43:16 -0700

    Finished outline of reprocess_data and io app

--- SiPMStudio/io/reprocess_files.py
+++ SiPMStudio/io/reprocess_files.py
@@ -1,8 +1,22 @@
+import glob
 import json
 import argparse
 
 from SiPMStudio.processing.processor import Processor, load_functions
 from SiPMStudio.processing.reprocess_data import reprocess_data
+
+def load_functions(file_name, proc_dict, processor):
+    for key, file_dict in proc_dict["processes"].items():
+        for name in file_dict.keys():
+            if name == file_name:
+                processor.add(key, file_dict[name])
+
+def reprocess(settings_dict, proc_dict, processor):
+    file_path = settings_dict["output_path_t2"]
+    file_list = glob.glob(f"{file_path}/*.h5")
+    for file_name in file_list:
+        load_functions(file_name, proc_dict, processor)
+        reprocess_data(settings_dict, processor, file_name)
 
 def main():
 
@@ -14,7 +28,14 @@ def main():
 
     settings_file = args.settings
     proc_file = args.procs
-    
+
+    settings_dict = None
+    with open(settings_file, "r") as json_file:
+        settings_dict = json.load(json_file)
+
+    proc_dict = None
+    with open(proc_file, "r") as json_file:
+        proc_dict = json.load(json_file)
 
 
 if __name__ == "__main__":
--- SiPMStudio/processing/reprocess_data.py
+++ SiPMStudio/processing/reprocess_data.py
@@ -19,14 +19,17 @@ def output_chunk(output, h5_file, begin, end):
         h5_file[key][begin:end] = value
 
 
-def reprocess_data(settings, processor, verbose=False, chunk=2000, write_size=1):
+def reprocess_data(settings, processor, file_name=None, verbose=False, chunk=2000, write_size=1):
     path_t2 = settings["output_path_t2"]
     output_files = []
 
-    base_name = settings["file_base_name"]
-    for entry in settings["init_info"]:
-        bias_label = entry["bias"]
-        output_files.append(f"t2_{base_name}_{bias_label}.h5")
+    if file_name is None:
+        base_name = settings["file_base_name"]
+        for entry in settings["init_info"]:
+            bias_label = entry["bias"]
+            output_files.append(f"t2_{base_name}_{bias_label}.h5")
+    else:
+        output_files.append(os.path.join(path_t2, file_name))
 
     for idx, file in enumerate(output_files):
         destination = os.path.join(path_t2, file)
