242f6396ac5df57ee52338252a54bb9dab2af155
nickruof <nickruof@uw.edu>
2021-10-06 16:02:45 -0700

    Update process_files

--- SiPMStudio/io/process_files.py
+++ SiPMStudio/io/process_files.py
@@ -5,21 +5,7 @@ from SiPMStudio.processing.processor import Processor, load_functions
 from SiPMStudio.processing.process_data import process_data
 
 
-def main():
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--settings", help="settings file name")
-    parser.add_argument("--procs", help="processor settings file name")
-    parser.add_argument("--bias", help="list of biases to process, comma separated", default=None)
-    parser.add_argument("--verbose", help="print extra output at runtime", type=bool)
-    args = parser.parse_args()
-    
-    settings_file = args.settings
-    proc_file = args.procs
-    bias = None
-
-    if args.bias:
-        bias = [int(i) for i in args.bias.split(",")]
+def process_files(settings_file, proc_file, bias=None, overwrite=True, chunk=4000, write_size=2, verbose=True):
 
     settings_dict = None
     with open(settings_file, "r") as json_file:
@@ -31,8 +17,20 @@ def main():
 
     processor = Processor()
     load_functions(proc_dict, processor)
-    process_data(settings_dict, processor, bias=bias, overwrite=True, chunk=4000, write_size=2, verbose=True)
+    process_data(
+        settings_dict, 
+        processor, bias=bias, overwrite=overwrite, chunk=chunk, 
+        write_size=write_size, 
+        verbose=verbose
+    )
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--settings", help="settings file name")
+    parser.add_argument("--procs", help="processor settings file name")
+    parser.add_argument("--bias", help="list of biases to process, comma separated", default=None)
+    parser.add_argument("--verbose", help="print extra output at runtime", type=bool)
+    args = parser.parse_args()
+
+    process_files(args.settings, args.procs, args.bias, verbose=args.verbose)
