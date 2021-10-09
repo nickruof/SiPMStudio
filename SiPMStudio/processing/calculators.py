4909239dd100fb0b3f65f2cb5302f57fc4bd69c3
Nicholas Ruof <nickruof@uw.edu>
2021-10-08 17:29:06 -0700

    Normalize charge calculator

--- SiPMStudio/processing/calculators.py
+++ SiPMStudio/processing/calculators.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+from SiPMStudio.analysis.dark import current_waveforms, integrate_current
 
 def normalize_energy(params_data, pc_peaks, label, waves_data=None):
     diffs = pc_peaks[1:] - pc_peaks[:-1]
@@ -12,6 +13,17 @@ def normalize_energy(params_data, pc_peaks, label, waves_data=None):
     output_params_data.loc[:, label] = norm_e
     return output_params_data
 
+
+def normalize_charge(outputs, wf_in, out, gain, int_window, peak_locs=None):
+    x0 = 0
+    waveforms = outputs[wf_in]
+    if peak_locs is None:
+        x0 = peak_locs[0]
+
+    current_forms = current_waveforms(waveforms)
+    charges = integrate_current(current_forms, int_window[0], int_window[1])
+    outputs[out] = (charges - x0) / gain
+    
 
 
 
