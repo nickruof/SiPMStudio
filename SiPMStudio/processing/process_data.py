e54b8e800446dc7b0bdf6d0f7cef02df168870b4
Nicholas Ruof <nickruof@uw.edu>
2021-10-12 20:30:54 -0700

    Rewrote process_data so all data is outputed for one loop through chunk keys

--- SiPMStudio/processing/process_data.py
+++ SiPMStudio/processing/process_data.py
@@ -53,13 +53,13 @@ def process_data(settings, processor, bias=None, overwrite=False, verbose=False,
             print(f"Processing: {file}")
         h5_file = h5py.File(destination, "r")
         num_rows = h5_file["/raw/timetag"][:].shape[0]
-        df_storage = {}
+        data_storage = {"size": 0}
         for i in tqdm_range(0, num_rows//chunk + 1):
             begin, end = _chunk_range(i, chunk, num_rows)
             wf_chunk = h5_file["/raw/waveforms"][begin:end]
             time_chunk = h5_file["/raw/timetag"][begin:end]
             output_data = _process_chunk(wf_chunk, time_chunk, processor=processor)
-            _output_chunk(h5_file, output_destination, output_data, df_storage, write_size, num_rows, chunk, end)
+            _output_chunk(output_destination, output_data, data_storage, write_size, num_rows, chunk, end)
             processor.reset_outputs()
         _copy_to_t2(
                 ["/raw/timetag", "/raw/dt", "bias"], 
@@ -88,22 +88,27 @@ def _process_chunk(wf_chunk, time_chunk, processor, rows=None):
     return processor.process()
 
 
-def _output_chunk(data_file, output_file, chunk_data, storage, write_size, num_rows, chunk, stop):
-    for output in chunk_data.keys():
+def _output_chunk(output_file, chunk_data, storage, write_size, num_rows, chunk, stop):
+    output_to_file = False
+    if (write_size == 1) | (num_rows < chunk):
+        output_to_file = True
+    elif stop >= num_rows-1:
+        output_to_file = True
+    elif storage["size"] == (write_size - 1):
+        output_to_file = True
+
+    for i, output in enumerate(chunk_data.keys()):
         if output not in storage:
             storage[output] = []
-        if (write_size == 1) | (num_rows < chunk):
-            _output_to_file(data_file, output_file, chunk_data, output)
-        else:
-            if stop >= num_rows-1:
-                storage[output].append(chunk_data[output])
-                _output_to_file(data_file, output_file, storage, output)
-                storage.clear()
-            else:
-                storage[output].append(chunk_data[output])
-                if len(storage) == write_size:
-                    _output_to_file(data_file, output_file, storage, output)
-                    storage.clear()
+        storage[output].append(chunk_data[output])
+        if i == 0:
+            storage["size"] = len(storage[output])
+        if output_to_file:
+            storage[output] = np.concatenate(storage[output])
+    if output_to_file:
+        _output_to_file(output_file, storage)
+        storage.clear()
+        storage["size"] = 0
 
 
 def _copy_to_t2(raw_names, process_names, h5_file, output_destination):
@@ -114,23 +119,20 @@ def _copy_to_t2(raw_names, process_names, h5_file, output_destination):
             output_file.create_dataset(process_names[i], data=h5_file[name])
 
 
-def _output_to_file(data_file, output_filename, storage, output_name):
-    output_data = None
-    if isinstance(storage[output_name], list):
-        output_data = np.concatenate(storage[output_name])
-    else:
-        output_data = storage[output_name]
+def _output_to_file(output_filename, storage):
     with h5py.File(output_filename, "a") as output_file:
-        if output_name in output_file:
-            output_file[output_name].resize(output_file[output_name].shape[0]+output_data.shape[0], axis=0)
-            output_file[output_name][-output_data.shape[0]:] = output_data
-        else:
-            if len(output_data.shape) == 2:
-                output_file.create_dataset(output_name, data=output_data, maxshape=(None, None))
-            elif len(output_data.shape) == 1:
-                output_file.create_dataset(output_name, data=output_data, maxshape=(None,))
+        for key, data in storage.items():
+            if key == "size": continue
+            if key in output_file:
+                output_file[key].resize(output_file[key].shape[0]+data.shape[0], axis=0)
+                output_file[key][-data.shape[0]:] = data
             else:
-                raise ValueError(f"Dimension of output data {output_data.shape} must be 1 or 2")
+                if len(data.shape) == 2:
+                    output_file.create_dataset(key, data=data, maxshape=(None, None))
+                elif len(data.shape) == 1:
+                    output_file.create_dataset(key, data=data, maxshape = (None,))
+                else:
+                    raise ValueError(f"Dimension of output data {data.shape} must be 1 or 2")
 
 
 def _output_date(output_destination, label=None):
