1123cec40ff1a15cdc49124b9f3436e50d2b25bc
Nicholas Ruof <nickruof@uw.edu>
2021-10-27 13:51:45 -0700

    remove channel dependence for some parameters

--- SiPMStudio/processing/process_metadata.py
+++ SiPMStudio/processing/process_metadata.py
@@ -28,19 +28,32 @@ def process_metadata(settings, digitizer, output_dir=None, verbose=False):
                     event_rows.append(event)
                     waveform_rows.append(waveform)
                     event_data_bytes = metadata_file.read(event_size)
-            _output_to_h5file(file_name, settings["file_base_name"], settings["output_path_raw"],
+            _output_to_h5file(file_name, settings["file_base_name"],
+                              settings["output_path_raw"], np.array(event_rows), digitizer)
+            _output_per_waveforms(file_name, settings["file_base_name"], settings["output_path_raw"],
                             np.array(event_rows), np.array(waveform_rows), file_names["channels"][i], digitizer)
     _output_time(time.time() - start)
 
 
-def _output_to_h5file(data_file, output_name, output_path, events, waveforms, channel, digitizer):
+def _output_per_waveforms(data_file, output_name, output_path, events, waveforms, channel, digitizer):
     bias = data_file["bias"]
     destination = os.path.join(output_path, f"raw_{output_name}_{bias}.h5")
     with h5py.File(destination, "w") as output_file:
-        output_file.create_dataset(f"/raw/{channel}/timetag", data=events.T[0])
-        output_file.create_dataset(f"/raw/dt", data=digitizer.get_dt())
         output_file.create_dataset(f"/raw/{channel}/energy", data=events.T[1])
         output_file.create_dataset(f"/raw/{channel}/waveforms", data=waveforms)
-        output_file.create_dataset("bias", data=float(data_file["bias"]))
-        output_file.create_dataset("adc_to_v", data=digitizer.v_range/2**digitizer.adc_bitcount)
-        output_file.create_dataset("date", data=time.time())
+
+
+def _output_to_h5file(data_file, output_name, output_path, events, digitizer):
+    bias = data_file["bias"]
+    destination = os.path.join(output_path, f"raw_{output_name}_{bias}.h5")
+    with h5py.File(destination, "w") as output_file:
+        if "/raw/timetag" not in output_file.keys():
+            output_file.create_dataset(f"/raw/timetag", data=events.T[0])
+        if "/raw/dt" not in output_file.keys():
+            output_file.create_dataset(f"/raw/dt", data=digitizer.get_dt())
+        if "bias" not in output_file.keys():
+            output_file.create_dataset("bias", data=float(data_file["bias"]))
+        if "adc_to_v" not in output_file.keys():
+            output_file.create_dataset("adc_to_v", data=digitizer.v_range/2**digitizer.adc_bitcount)
+        if "date" not in output_file.keys():
+            output_file.create_dataset("date", data=time.time())
