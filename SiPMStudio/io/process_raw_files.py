7ae4cb56b157fe48c0b71093529443be545fabf9
Nicholas Ruof <nickruof@uw.edu>
2022-04-06 11:08:20 -0700

    Add io option to use csv input files

--- SiPMStudio/io/process_raw_files.py
+++ SiPMStudio/io/process_raw_files.py
@@ -3,23 +3,27 @@ import json
 import argparse
 
 from SiPMStudio.core.digitizers import CAENDT5730
-from SiPMStudio.processing.process_metadata import process_metadata
+from SiPMStudio.processing.process_metadata import process_metadata, process_metadata_csv
 
 
-def process_raw_files(settings_file, compass="v1"):
+def process_raw_files(settings_file, compass="v1", csv=False):
 
     settings_dict = None
     with open(settings_file, "r") as json_file:
         settings_dict = json.load(json_file)
 
     digitizer = CAENDT5730(compass=compass)
-    process_metadata(settings_dict, digitizer, verbose=True)
+    if csv is True:
+        process_metadata_csv(settings_dict, digitizer, verbose=True)
+    else:
+        process_metadata(settings_dict, digitizer, verbose=True)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--settings", help="settings file name")
     parser.add_argument("--compass", help="compass version (v1 or v2)", default="v1")
+    parser.add_argument("--csv", help="set True if using csv input files", default=False)
     args = parser.parse_args()
 
-    process_raw_files(args.settings, args.compass)
+    process_raw_files(args.settings, args.compass, args.csv)
